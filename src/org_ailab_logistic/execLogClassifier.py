2c09329c34a0f45d7092d4a3554ce81c57a9ca06
superhy <superhy@superhy-PC>
2014-09-21 10:49:32 +0800

    log_exec(2014.9.21)

--- src/org_ailab_logistic/execLogClassifier.py
+++ src/org_ailab_logistic/execLogClassifier.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import logRegres
+from numpy.core.numeric import array
 
 def gradAscent():
     dataArr, labelMat = logRegres.loadDataSet()
@@ -9,10 +10,29 @@ def gradAscent():
     
     logRegres.plotBestFit(weights.getA())
     
+def stocGradAscent0():
+    dataArr, labelMat = logRegres.loadDataSet()
+    weights = logRegres.stocGradAscent0(array(dataArr), labelMat);
+    print weights
+    
+    logRegres.plotBestFit(weights);
+    
+def stocGradAscent1(numIter):
+    dataArr, labelMat = logRegres.loadDataSet()
+    weights = logRegres.stocGradAscent1(array(dataArr), labelMat, numIter);
+    print weights
+    
+    logRegres.plotBestFit(weights);
+    
 def test():
     testId = int(raw_input('输入数据：'))
     if testId == 0:
         gradAscent()
+    elif testId == 1:
+        stocGradAscent0();
+    elif testId == 2:
+        numIter = int(raw_input('输入参数：'))
+        stocGradAscent1(numIter)
 
 if __name__ == '__main__':
     test()
