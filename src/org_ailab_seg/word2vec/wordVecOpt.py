a5148201c40a9d71ad85da104e99837fb1ce5883
hylovedd <hylovedd@hylovedd-pc>
2016-03-17 23:43:14 +0800

    word_vec_encoding(2016.3.17)

--- src/org_ailab_seg/word2vec/wordVecOpt.py
+++ src/org_ailab_seg/word2vec/wordVecOpt.py
@@ -13,22 +13,37 @@ from Cython.Build.Cythonize import multiprocessing
 from gensim.models.word2vec import Word2Vec, LineSentence
 
 from org_ailab_load import loadLocalFileData
+from org_ailab_seg.advanceSegOpt import advanceSegOpt
 
 
 class wordVecOpt:
-    def __init__(self, corpusFile, modelPath):
-        self.corpusFile = corpusFile
+    def __init__(self, modelPath, _size=100, _window=5, _minCount=1, _workers=multiprocessing.cpu_count()):
         self.modelPath = modelPath
+        self._size = _size
+        self._window = _window
+        self._minCount = _minCount
+        self._workers = _workers
         
-    def trainWord2VecModel(self, _size=100, _window=5, _minCount=5, _workers=multiprocessing.cpu_count()):
-        fileType = loadLocalFileData.checkFile(self.corpusFile)
-        print(fileType)
+    def trainWord2VecModel(self, corpusFile):
+        advanceSegOpt().reLoadEncoding()
+        fileType = loadLocalFileData.checkFile(corpusFile)
         if fileType == u'null' or fileType == u'error':
             warnings.warn('load file error!')
         else:
-            model = Word2Vec(LineSentence(self.corpusFile), size=_size, window=_window, min_count=_minCount, workers=_workers)
+            model = Word2Vec(LineSentence(corpusFile), size=self._size, window=self._window, min_count=self._minCount, workers=self._workers)
             model.save(self.modelPath)
             model.init_sims(replace=True)
+            print('producing word2vec model ... ok')
             return model
+    
+    def queryMostSimilarWordVec(self, model, wordStr):
+        wordSimilarList = model.most_similar(wordStr.decode('utf-8'))
+        return wordSimilarList
+    
+    def queryMsimilarWVfromFile(self, modelFilePath, wordStr):
+        advanceSegOpt().reLoadEncoding()
+        model = Word2Vec.load(modelFilePath)
+        return self.queryMostSimilarWordVec(model, wordStr)
+    
 if __name__ == '__main__':
     pass
--- test/test_ailab_seg/word2vec/wordVecOptTest.py
+++ test/test_ailab_seg/word2vec/wordVecOptTest.py
@@ -6,14 +6,31 @@ Created on 2016年3月17日
 @author: superhy
 '''
 from org_ailab_seg.word2vec.wordVecOpt import wordVecOpt
+from org_ailab_seg.advanceSegOpt import advanceSegOpt
 
 
 def testTrainWord2VecModel():
+    corpusFilePath = u"../segNLPCC2014.txt"
+    corpusFile = open(corpusFilePath, u'r')
+    modelPath = u'NLPCC2014word2vecModel.vector'
+    wordVecOptObj = wordVecOpt(modelPath)
+    model = wordVecOptObj.trainWord2VecModel(corpusFilePath)
+    wordStr = u'调戏/v'
+    print(u'word vec object: ' + wordStr)
+    queryList = wordVecOptObj.queryMostSimilarWordVec(model, wordStr)
+    for e in queryList:
+        print e[0], e[1]
+    
+def testQueryWordVec():
+    advanceSegOptObj = advanceSegOpt()
+    advanceSegOptObj.reLoadEncoding()
     filePath = u"../segNLPCC2014.txt"
     file = open(filePath, u'r')
-    modelPath = u'NLPCC2014word2vecModel.txt'
+    modelPath = u'NLPCC2014word2vecModel.vector'
     wordVecOptObj = wordVecOpt(filePath, modelPath)
-    wordVecOptObj.trainWord2VecModel()
+    wordStr = u'调戏'
+    wordVecOptObj.queryMsimilarWVfromFile(modelPath, wordStr)
     
 if __name__ == '__main__':
     testTrainWord2VecModel()
+    #testQueryWordVec()
