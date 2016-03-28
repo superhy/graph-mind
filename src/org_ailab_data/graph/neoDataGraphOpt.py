a1333fbc7d6b0bd3f38a80e2f891f27628f7f1f9
hylovedd <hylovedd@hylovedd-pc>
2016-03-29 03:09:45 +0800

    neo4j_test(2016.3.29)

--- src/org_ailab_data/graph/neoDataGraphOpt.py
+++ src/org_ailab_data/graph/neoDataGraphOpt.py
@@ -7,26 +7,46 @@ Created on 2016年3月23日
 '''
 
 from py2neo import Node, Relationship
+from py2neo.database import Graph
+from py2neo.database.auth import authenticate
 
-neoHost = "http://localhost:7474/db/data/"
+
+_user = "neo4j"
+_password = "qdhy199148"
+_database = "http://localhost:7474/db/data/"
 
 class neoGraphOpt:
-    def __init__(self):
-        self.host = neoHost
+    def __init__(self, host=None, http_port=None, bolt_port=None, user=_user, password=_password, datebase=_database):
+        self.host = host
+        self.http_port = http_port
+        self.bolt_port = bolt_port
+        self.user = user;
+        self.password = password
+        self.datebase = datebase
+        
+    def connectGraph(self):
+        #graph = Graph(u'http://neo4j:<qdhy199148>@localhost:7474/db/data/')
+        authenticate("localhost:7474", "neo4j", "qdhy199148")
+        graph = Graph("http://localhost:7474/db/data/")
+        #graph = Graph(user = self.user, password = self.password)
+        return graph
         
     def createNode(self, nodeType, nodeName):
-        pass
+        return Node(nodeType, name=nodeName)
     
-    def createRelationship(self, relationship):
-        pass
+    def createRelationship(self, relationship, node1, node2):
+        return Relationship(node1, relationship, node2)
     
     def createNodeAndRelats(self, nodeTyep1, nodeName1, nodeTyep2, nodeName2, relationship):
-        node1 = Node(nodeTyep1, name = nodeName1)
-        node2 = Node(nodeTyep2, name = nodeName2)
-        relat = Relationship(node1, relationship, node2)
+        node1 = self.createNode(nodeTyep1, nodeName1)
+        node2 = self.createNode(nodeTyep2, nodeName2)
+        relat = self.createRelationship(relationship, node1, node2)
         
         return relat
 
 if __name__ == '__main__':
     neoObj = neoGraphOpt()
     print(neoObj.createNodeAndRelats("teacher", "huangqingsong", "student", "huyang", "teach"))
+    
+    test_graph = neoObj.connectGraph()
+    print test_graph
