afdbb224de06065e5cb1bb3eb3f5e7194007d2b6
superhy <superhy@superhy-PC>
2016-04-26 13:14:37 +0800

    word_pos_global

---
+++ src/org_ailab_io/global/WORD_POS.py
@@ -0,0 +1,58 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 2016年4月20日
+
+@author: superhy
+'''
+
+
+# noun(名词)
+noun = [u'n', u'nr', u'ns', u'nt', u'nz',
+        u'nl', u'ng']
+
+# time(时间词)
+time = [u't', u'tg']
+
+# place(处所词)
+place = [u's']
+
+# verb(动词)
+verb = [u'v', u'vd', u'vn', u'vf', u'vx'
+        u'vi', u'vl', u'vg']
+
+# adjective(形容词)
+adj = [u'a', u'ad', u'an', u'ag', u'al']
+
+# distinguish(区别词)
+dist = [u'b']
+
+# state(状态词)
+state = [u'z']
+
+# pronoun(代词)
+pronoun = [u'r', u'rr', u'ry', u'rg']
+
+# math(数词)
+math = [u'm', u'mq']
+
+# quantifiers(量词)
+quantifier = [u'q']
+
+# adverb(副词)
+adv = [u'd']
+
+# preposition(介词)
+preposition = [u'p']
+
+# conjunction(连词)
+conjunction = [u'c']
+
+# auxiliary(助词)
+auxiliary = [u'u', u'uz', u'ul', u'ug', u'ud',
+             u'us', u'uj', u'uy', u'uv']
+
+# modal(语气词)
+modal = [u'e', u'y', u'o']
+
+# symbol(符号)
+symbol = [u'h', u'k', u'x', u'w']
---
+++ test/test_ailab_seg/wordExtraTestUnit.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 2016年4月20日
+
+@author: superhy
+'''
+from org_ailab_seg.extraSegOpt import extraSegOpt
+
+
+def countWordPos():
+    extraSegOpt().reLoadEncoding()
+    
+    segFile = open(u'segNLPCC2014.txt', 'r')
+    segLines = segFile.readlines()
+    dic = {}
+    for line in segLines:
+        words = line.split(' ')
+        for word in words:
+            if word.find('/') != -1:
+                word_str = word.split('/')[0]
+                seg = word.split('/')[1]
+                if dic.has_key(seg) and len(dic[seg]) < 5:
+                    dic[seg].append(word_str)
+                elif not dic.has_key(seg):
+                    dic[seg] = [word_str]
+#                 else:
+#                     print(seg + ": "),
+#                     for s in dic[seg]:
+#                         print(s.encode('utf-8') + ';'),
+#                     print('')
+    segFile.close()
+    
+    for key in dic:
+        if key.startswith('x'):
+            print(key + ": "),
+            for s in dic[key]:
+                print(s.encode('utf-8') + ';'),
+            print('')
+    
+#     posListFileStr = ''
+#     fileWriteObj = open(u'NLPCC2014WordPosList.txt', 'w')
+#     for key in dic:
+#         posListFileStr += (key + ': [')
+#         for w in dic[key]:
+#             posListFileStr += (w + ';')
+#         posListFileStr += ']\n'
+#     fileWriteObj.write(posListFileStr)
+#     fileWriteObj.close()
+
+if __name__ == '__main__':
+    countWordPos()
